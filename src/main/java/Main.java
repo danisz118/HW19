79c89be665641eb04fad7d5b9ae52272a9f7ac86
danisz118 <daniz118@gmail.com>
2020-04-05 11:34:02 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+            File file = new File(".");
+            listFiles(file);
+        }
+        private static void listFiles(File file) {
+            for (File files : file.listFiles()) {
+                if (files.isFile()) {
+                    System.out.println(files);
+                }
+            }
+    }
+}
